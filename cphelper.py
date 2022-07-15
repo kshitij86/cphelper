b891e391fbfe988ba96df5532b7501fbf70259c3
Kshitij <kshitij.kotasthane@gmail.com>
2022-07-15 11:22:02 +0530

    cleaning the source

--- cphelper.py
+++ cphelper.py
@@ -1,22 +1,15 @@
 import os
 import click
-from strings import DEFAULT_LANG, CODE_TEMPLATES, CURR_CONFIG, SUCCESS_MSG
+from strings import DEFAULT_LANG, CODE_TEMPLATES, CURR_CONFIG, SUCCESS_MSG, HELP_STRINGS
 
 
 @click.command()
-@click.option('--name', type=str, default='new_folder', 
-	help="The name of the directory, preferably the name of the contest.")
-@click.option('--count', type=int, default=6, 
-	help="The number of files to be created, defaults to 6.")
-@click.option('--lang', type=str, default='cpp', 
-	help="The programming language used, same for all files, defaults to C++. Only the extension needs to provided.")
-@click.option('--target', type=str, default='.', 
-	help="The directory where the create the folder, defaults to pwd.")
-@click.option("--template", type=bool, default=True, 
-	help="This populates each file with the default C++ template. Does not work if language is other than C++")
-@click.option("--verbose", type=bool, default=False,
-	help="This will make cphelper print messages to the console")
-def cli(name, count, lang, target, template, verbose):
+@click.option('--name', type=str, default='new_folder', help=HELP_STRINGS["NAME_HELP"])
+@click.option('--count', type=int, default=6, help=HELP_STRINGS["COUNT_HELP"])
+@click.option('--lang', type=str, default='cpp', help=HELP_STRINGS["LANG_HELP"])
+@click.option('--target', type=str, default='.', help=HELP_STRINGS["TARGET_HELP"])
+@click.option("--verbose", type=bool, default=False,help=HELP_STRINGS["VERBOSE_HELP"])
+def cli(name, count, lang, target, verbose):
 	"""This creates a folder with count files"""
 
 	# method to print messages only if verbose is set to True
@@ -29,32 +22,44 @@ def cli(name, count, lang, target, template, verbose):
 		console_print([name, lang, target, count])
 
 
-		# try to create the directory for the files if not exists
-		PATH_DIR = f"{target}/{name}/"
-		folder_exists = os.path.isdir(PATH_DIR)
-		if folder_exists == False:
-			os.mkdir(path=PATH_DIR)
+		try:
+			if len(name) > 25:
+				raise Exception("contest name too long, needs to be at  25 characters")
+			if count > 100:
+				raise Exception("yowza!, that's a lot of files. try again with atmost 100 files")
+
+		
+			# try to create the directory for the files if not exists
+			if os.path.isdir(target):
+				PATH_DIR = f"{target}/{name}/"
+				folder_exists = os.path.isdir(PATH_DIR)
+				if folder_exists == False:
+					os.mkdir(path=PATH_DIR)
+			else:
+				raise Exception(f"directory \"{target}\" not found")
 
 
-		# create 'count' number of files
-		# each file shuld have the predefined template depending on template option
-		# works only when language matches default lang
-		FILES_PREFIX = f".{lang}"
-		template_file = open(CODE_TEMPLATES, 'r')
-		cpp_template = template_file.read()
-		for i in range(int(count)):
-			file_name = PATH_DIR + f"{i+1}" + FILES_PREFIX
-			code_file = open(file_name, 'w')
+			# create 'count' number of files
+			# each file shuld have the predefined template depending on template option
+			# works only when language matches default lang
+			FILES_PREFIX = f".{lang}"
+			template_file = open(CODE_TEMPLATES, 'r')
+			cpp_template = template_file.read()
+			for i in range(int(count)):
+				file_name = f"{PATH_DIR}{i+1}{FILES_PREFIX}"
+				code_file = open(file_name, 'w')
+				if lang == DEFAULT_LANG:
+					code_file.write(cpp_template)
+
+			# create input and output files if lang is C++
 			if lang == DEFAULT_LANG:
-				code_file.write(cpp_template)
-
-		# create input and output files if lang is C++
-		if lang == DEFAULT_LANG:
-			input_file = f"{PATH_DIR}input.txt"
-			open(input_file, 'w')
-			output_file = f"{PATH_DIR}output.txt"
-			open(output_file, 'w')
+				input_file = f"{PATH_DIR}input.txt"
+				open(input_file, 'w')
+				output_file = f"{PATH_DIR}output.txt"
+				open(output_file, 'w')
 
 
-		console_print(SUCCESS_MSG)
+			console_print(SUCCESS_MSG)
+		except Exception as e:
+			click.echo(f"fatal error: {str(e)}")
 	execute()
--- strings.py
+++ strings.py
@@ -4,3 +4,11 @@ CODE_TEMPLATES = "code_templates/cpp_template.txt"
 SUCCESS_MSG = "created the directly successfully"
 TEST_SUCCESS = "Test success in creating folder, cleaning up"
 CLEANUP_SUCCESS = "Cleanup done, tests complete"
+
+HELP_STRINGS = {
+	"NAME_HELP": "The name of the directory, preferably the name of the contest.",
+	"COUNT_HELP": "The number of files to be created, defaults to 6.",
+	"LANG_HELP": "The programming language used, same for all files, defaults to C++. Only the extension needs to provided.",
+	"TARGET_HELP": "The directory where the create the folder, defaults to pwd.",
+	"VERBOSE_HELP": "This will make cphelper print messages to the console"
+}
