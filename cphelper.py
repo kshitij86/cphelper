a34df256115c8fd5f05ad1c93933225466cda134
Kshitij <kshitij.kotasthane@gmail.com>
2022-09-07 13:25:59 +0530

    fixed output file naming

--- cphelper.py
+++ cphelper.py
@@ -9,55 +9,54 @@ from strings import DEFAULT_LANG_EXT, CPP_TEMPLATE, CURR_CONFIG_PROMPT, SUCCESS_
 @click.option('--count', type=int, default=6, help=HELP_STRINGS["COUNT_HELP"])
 @click.option('--lang', type=str, default='cpp', help=HELP_STRINGS["LANG_HELP"])
 @click.option('--target', type=str, default='.', help=HELP_STRINGS["TARGET_HELP"])
-@click.option("--verbose", type=bool, default=False,help=HELP_STRINGS["VERBOSE_HELP"])
+@click.option("--verbose", type=bool, default=False, help=HELP_STRINGS["VERBOSE_HELP"])
 def cli(name, count, lang, target, verbose):
-	"""This creates a folder with count files"""
+    """This creates a folder with count files"""
 
-	# method to print messages only if verbose is set to True
-	def console_print(msg):
-		if verbose == True:
-			click.echo(msg)
+    # method to print messages only if verbose is set to True
+    def console_print(msg):
+        if verbose == True:
+            click.echo(msg)
 
-	def execute():
-		console_print(CURR_CONFIG_PROMPT)
-		console_print([name, lang, target, count])
+    def execute():
+        console_print(CURR_CONFIG_PROMPT)
+        console_print([name, lang, target, count])
 
-		try:
-			if len(name) > 25:
-				raise Exception("contest name too long, needs to be at  25 characters")
-			if count > 100:
-				raise Exception("yowza!, that's a lot of files. try again with atmost 100 files")
+        try:
+            if len(name) > 25:
+                raise Exception(
+                    "contest name too long, needs to be at  25 characters")
+            if count > 100:
+                raise Exception(
+                    "yowza!, that's a lot of files. try again with atmost 100 files")
 
-		
-			# try to create the directory for the files if not exists
-			if os.path.isdir(target):
-				PATH_DIR = f"{target}/{name}/"
-				folder_exists = os.path.isdir(PATH_DIR)
-				if folder_exists == False:
-					os.mkdir(path=PATH_DIR)
-			else:
-				raise Exception(f"directory \"{target}\" not found")
+            # try to create the directory for the files if not exists
+            if os.path.isdir(target):
+                PATH_DIR = f"{target}/{name}/"
+                folder_exists = os.path.isdir(PATH_DIR)
+                if folder_exists == False:
+                    os.mkdir(path=PATH_DIR)
+            else:
+                raise Exception(f"directory \"{target}\" not found")
 
+            # create 'count' number of files
+            # each file shuld have the predefined template depending on template option
+            # works only when language matches default lang
+            FILES_PREFIX = f".{lang}"
+            for i in range(int(count)):
+                file_name = f"{PATH_DIR}{i+1}{FILES_PREFIX}"
+                code_file = open(file_name, 'w')
+                if lang == DEFAULT_LANG_EXT:
+                    code_file.write(CPP_TEMPLATE)
 
-			# create 'count' number of files
-			# each file shuld have the predefined template depending on template option
-			# works only when language matches default lang
-			FILES_PREFIX = f".{lang}"
-			for i in range(int(count)):
-				file_name = f"{PATH_DIR}{i+1}{FILES_PREFIX}"
-				code_file = open(file_name, 'w')
-				if lang == DEFAULT_LANG_EXT:
-					code_file.write(CPP_TEMPLATE)
+            # create input and output files if lang is C++
+            if lang == DEFAULT_LANG_EXT:
+                input_file = f"{PATH_DIR}input.txt"
+                open(input_file, 'w')
+                output_file = f"{PATH_DIR}output.txt"
+                open(output_file, 'w')
 
-			# create input and output files if lang is C++
-			if lang == DEFAULT_LANG_EXT:
-				input_file = f"{PATH_DIR}input.txt"
-				open(input_file, 'w')
-				output_file = f"{PATH_DIR}output.txt"
-				open(output_file, 'w')
-
-
-			console_print(SUCCESS_MSG)
-		except Exception as e:
-			click.echo(f"fatal error: {str(e)}")
-	execute()
+            console_print(SUCCESS_MSG)
+        except Exception as e:
+            click.echo(f"fatal error: {str(e)}")
+    execute()
--- strings.py
+++ strings.py
@@ -7,11 +7,11 @@ CLEANUP_SUCCESS = "Cleanup done, tests complete"
 TEST_FOLDER_NAME = "test_contest"
 
 HELP_STRINGS = {
-	"NAME_HELP": "The name of the directory, preferably the name of the contest.",
-	"COUNT_HELP": "The number of files to be created, defaults to 6.",
-	"LANG_HELP": "The programming language used, same for all files, defaults to C++. Only the extension needs to provided.",
-	"TARGET_HELP": "The directory where the create the folder, defaults to pwd.",
-	"VERBOSE_HELP": "This will make cphelper print messages to the console"
+    "NAME_HELP": "The name of the directory, preferably the name of the contest.",
+    "COUNT_HELP": "The number of files to be created, defaults to 6.",
+    "LANG_HELP": "The programming language used, same for all files, defaults to C++. Only the extension needs to provided.",
+    "TARGET_HELP": "The directory where the create the folder, defaults to pwd.",
+    "VERBOSE_HELP": "This will make cphelper print messages to the console"
 }
 
 CPP_TEMPLATE = "/*\n        __                           ______   ________\n_____  |  | _______    ____   ____  /  __  \\ /  _____/\n\\__  \\ |  |/ /\\__  \\  /    \\_/ __ \\ >      </   __  \\\n / __ \\|    <  / __ \\|   |  \\  ___//   --   \\  |__\\  \\\n(____  /__|_ \\(____  /___|  /\\___  >______  /\\_____  /\n     \\/     \\/     \\/     \\/     \\/       \\/       \\/\n\n\n */\n\n#include <bits/stdc++.h>\n#define ll long long\n#define MAX 1000003\n#define pii pair<int,int>\n#define VP vector< pii >\n#define MOD 1000000007\n#define mp make_pair\n#define pb push_back\n#define rep(i,a,b) for(int (i) = (a); (i) < (b); (i)++)\n#define all(v) (v).begin(),(v).end()\n#define S(x) scanf(\"%d\",&(x))\n#define S2(x,y) scanf(\"%d%d\",&(x),&(y))\n#define SL(x) scanf(\"%lld\",&(x))\n#define SL2(x) scanf(\"%lld%lld\",&(x),&(y))\n#define P(x) printf(\"%d\\n\",(x))\n#define FF first\n#define SS second\n\nusing namespace std;\nint main() {\n#ifndef ONLINE_JUDGE\n\tfreopen(\"input1.txt\", \"r\", stdin);\n\tfreopen(\"output1.txt\", \"w\", stdout);\n#endif\n\n\tll n, t;\n\tcin >> t;\n\twhile (t--) {\n\t\tcin >> n;\n\t}\n\n}"
